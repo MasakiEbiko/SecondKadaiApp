d45d2b01311caeb3f7b810475058bd5e2480fbfb
masakiEbiko <ebiko@kyoei-.com>
2024-09-26 14:17:18 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by mba2408.spacegray kyoei.engine on 2024/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
